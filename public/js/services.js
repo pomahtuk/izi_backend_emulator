f29af9b7c79523e0235874fdd13d320bde64d87d
Ilja Lovrjakov <pman89@ya.ru>
2013-12-04 16:38:47 +0400

    ~~~

--- public/js/services.js
+++ public/js/services.js
@@ -189,9 +189,9 @@
     };
   }).service("backendWrapper", function($http, ngProgress, $location, $i18next) {
     return {
-      museum_id: "529ee18e13b106fb0f000002",
-      content_provider_id: "529ee18e13b106fb0f000001",
-      backend_url: "http://192.168.158.128:3000/api",
+      museum_id: "528f05b3c99772031a000002",
+      content_provider_id: "528f05b3c99772031a000001",
+      backend_url: "http://prototype.izi.travel/api",
       museums: [],
       exhibits: [],
       modal_translations: [],
